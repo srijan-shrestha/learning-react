dded63d12ceaff3953870310d5c7d7524e71cb13
Srijan Shrestha <srijaniw@gmail.com>
2020-09-16 22:23:52 +0545

    Setup Fontawesome icon library Add navigation component Workin on navigation feature

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Naviagtion from './Navigation'
 
 function Header() {
     return (
         <div>
-            <header className="border-b font-bold p-3">
-                App name
+            <header className="border-b p-3 flex justify-between items-center">
+                <span className="font-bold"> App Name </span>
+                <Naviagtion />
             </header>
         </div>
     );

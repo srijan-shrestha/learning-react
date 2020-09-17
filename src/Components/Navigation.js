86df596a7737ba3494b5561fa720b54bc49aa57d
Srijan Shrestha <srijaniw@gmail.com>
2020-09-17 20:44:08 +0545

    Change react version Add react spring animation Using atransition animation for navbar

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,36 +1,48 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-
+import {useTransition, animated} from 'react-spring'
 
 function Navigation() {
     const [showMenu, setShowMenu] = useState(false)
-    let menu
-    let menuMask
+    const transitions = useTransition(showMenu, null, {
+        from: { position: 'absolute', opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+    })
+    // className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow" menu
+    // className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50" menu mask
+    // let menu
+    // let menuMask
 
-    if(showMenu) {
-        menu = 
-        <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
-            The menu
-        </div>
-        menuMask = 
-        <div className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50">
-            onClick={() => setShowMenu(false) }  
-        </div>
-    }
+    // if(showMenu) {
+    //     menu = 
+    //     <div >
+    //         The menu
+    //     </div>
+    //     menuMask = 
+    //     <div 
+    //     onClick={() => setShowMenu(false) }>
+    //     </div>
+    // }
     return (
         <nav>
             <span className="text-xl">
-            <FontAwesomeIcon
-                icon={faBars}
-                onClick={() => setShowMenu(!showMenu) }
-            />
+                <FontAwesomeIcon
+                    icon={faBars}
+                    onClick={() => setShowMenu(!showMenu) }
+                />
             </span>
+            {
+                transitions.map(({ item, key, props }) =>
+                    item && <animated.div key={key} style={props}>✌️</animated.div>
+                )
+            }
             
-            { menuMask }
-            { menu }
+            {/* { menuMask }
+            { menu } */}
         </nav>
-    );
+    )
 }
 
-export default Navigation;
+export default Navigation

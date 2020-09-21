ed45133c33d8c70e3bbf5be30d543aee8c02ef5a
Srijan Shrestha <srijaniw@gmail.com>
2020-09-21 22:23:02 +0545

    Change home component to fecth 10 products from api Use loading and error handling in it

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,11 +1,54 @@
-import React from 'react'
-import  HelloWorld from '../Components/HelloWorld';
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import Loader from '../Components/Loader'
 
 function Home() {
+    const [products, setProducts] = useState({
+        loading: false,
+        data: null,
+        error: false
+    })
+    // Create your own Mock API: https://mockapi.io/
+    const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products?page=1&limit=10`
+
+    useEffect(() => {
+        setProducts({
+            loading: true,
+            data: null,
+            error: false
+        })
+        axios.get(url).then(response =>{
+            setProducts({
+                loading: false,
+                data: response.data,
+                error: false
+            })
+        })
+        .catch(error => {
+            setProducts({
+                loading: false,
+                data: null,
+                error: true
+            })
+        })
+    }, [url])
+    let content = null
+
+    if(products.error) {
+        content = <p>
+            There was an error fetching the product.
+        </p>
+    }
+
+    if(products.loading) {
+        content = <Loader />
+    }
+
     return (
         <div>
-            <h1 className="font-bold text-2xl mb-3">This is the home page</h1>
-            <HelloWorld name="Srijan"/>
+            <h1 className="font-bold text-2xl mb-3">Best Sellers</h1>
+            {content}
         </div>
     )
 }

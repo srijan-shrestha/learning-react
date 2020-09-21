d980d0d9ca86b8bd368d69cd14dac5bf89866ffa
Srijan Shrestha <srijaniw@gmail.com>
2020-09-21 22:01:16 +0545

    Make loading text to appear while data is fetched from api Add new loader component

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -4,43 +4,57 @@ import axios from 'axios'
 
 function Product() {
     const { id } = useParams()
-    const [product, setProduct] = useState(null)
+    const [product, setProduct] = useState({
+        loading: false,
+        data: null
+    })
     // Create your own Mock API: https://mockapi.io/
     const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/${id}`
     let content = null
     
     useEffect(() => {
+        setProduct({
+            loading: true,
+            data: null
+        })
         axios.get(url).then(response =>{
-            setProduct(response.data)
+            setProduct({
+                loading: false,
+                data: response.data
+            })
         })
     }, [url])
 
-    if (product) {
+    if(product.loading) {
+        content = <p>...loading</p>
+    }
+
+    if (product.data) {
         return (
             content = 
             <div>
                 <h1 className="font-bold text-2xl mb-3" >
-                    {product.name}
+                    {product.data.name}
                 </h1>
                 <div>
                     <img
-                        src={product.images[0].imageUrl}
-                        alt={product.name}
+                        src={product.data.images[0].imageUrl}
+                        alt={product.data.name}
                     />
                 </div>
                 <div className="font-bold text-xl mb-3">
-                    $ {product.price}
+                    $ {product.data.price}
                 </div>
                 <div>
-                    {product.description}
+                    {product.data.description}
                 </div>
             </div>
         )
     }
    
     return (
-        <div>
-            
+        <div className="container mx-auto">
+            {content}
         </div>
     )
    

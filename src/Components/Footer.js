a533a13b334f564008dce32e254bdb76e9350db7
Srijan Shrestha <srijaniw@gmail.com>
2020-09-22 21:19:21 +0545

    Made footer fixed to the bottom

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <div>
-            <header className="bg-gray-200 text-center text-xs p-3 absolute bottom-0 w-full">
+            <header className="bg-gray-200 text-center text-xs p-3 fixed bottom-0 w-full">
                &copy; Copyright 2020
             </header>
         </div>
